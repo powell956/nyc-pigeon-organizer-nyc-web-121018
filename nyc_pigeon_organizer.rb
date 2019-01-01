e9fecdb3e6c978045b5cd1452552f34d14296bbc
Dan Powell <powell956@gmail.com>
2019-01-01 07:28:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -28,9 +28,9 @@ def nyc_pigeon_organizer(data)
     list.each do |attribute, names|  #'purple', names array
       names.each do |name|
         if pigeon_list[name]
-          pigeon_list[name].store(category, [])
+          pigeon_list[name].store(category, {})
         else
-          pigeon_list[name] = { category => [] }
+          pigeon_list[name] = { category => {} }
         end
       end
     end
