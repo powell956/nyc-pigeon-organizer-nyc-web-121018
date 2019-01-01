fb5af71cfeb604f41840d8fad974cf6b3e4cae24
Dan Powell <powell956@gmail.com>
2019-01-01 05:54:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,31 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+
+  pigeon_list = {}
+
+  #gather attribute categories by name
+  data.each do |category, list|
+    list.each do |attribute, names|
+      names.each do |name|
+        if pigeon_list[name]
+          pigeon_list[name].store(list, [])
+        else
+          pigeon_list[name] = { list => [] }
+        end
+      end
+    end
+  end
+
+  #fill in categories
+  data.each do |category, list|
+    list.each do |attribute, names|
+      names.each do |name|
+        if pigeon_list[name]
+          pigeon_list[name].store(list, [])
+        else
+          pigeon_list[name] = { list => [] }
+        end
+      end
+    end
+  end
+
 end
